8901076922931910f4d6cda2c0135c4b77373f24
swapnilkuthe <swapnilkuthe20@gmail.com>
2025-01-04 22:17:17 +0530

    day 7 scenario done

---
+++ 7_multiTable.js
@@ -0,0 +1,10 @@
+// Write a js function that take a no. and print multiplication table.
+
+function multiTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${num} * ${i} =`, num * i);
+    }
+}
+multiTable(5);
+
+
